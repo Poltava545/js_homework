081da5bbeb650434ee8f4619faf550d063732b20
Oleksandr Poltavets <poltavets04@gmail.com>
2023-11-14 13:49:08 +0200

    js_hw_4

---
+++ jsHW1.js
@@ -0,0 +1,14 @@
+function drawTriangle(height, symbol) {
+    for (let i = 1; i <= height; i++) {
+        console.log(symbol.repeat(i));
+    }
+}
+drawTriangle(5, '*');
+
+
+/*
+let height = 5;
+let symbol = '*';
+for (let i = 1; i <= height; i++) 
+  {console.log(symbol.repeat(i));}
+*/
